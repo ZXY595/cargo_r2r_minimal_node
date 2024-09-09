9b665085bbce3e4e64ca42a48851003ea559a017
ZXY <3303057136@qq.com>
2024-09-10 00:20:44 +0800

    new file:   .cargo/config.toml 	modified:   .gitignore 	new file:   CMakeLists.txt 	renamed:    r2r_minimal_node/Cargo.lock -> Cargo.lock 	renamed:    r2r_minimal_node/Cargo.toml -> Cargo.toml 	renamed:    r2r_minimal_node_msgs/action/SayHello.action -> action/SayHello.action 	modified:   cargo-generate.toml 	renamed:    r2r_minimal_node/dummy.c -> dummy.c 	renamed:    r2r_minimal_node_msgs/msg/Hello.msg -> msg/Hello.msg 	new file:   package.xml 	modified:   pre-script.rhai 	renamed:    r2r_minimal_node/r2r_cargo.cmake -> r2r_cargo.cmake 	deleted:    r2r_minimal_node/.cargo/config.toml 	deleted:    r2r_minimal_node/CMakeLists.txt 	deleted:    r2r_minimal_node/package.xml 	deleted:    r2r_minimal_node/src/main.rs 	deleted:    r2r_minimal_node_msgs/CMakeLists.txt 	deleted:    r2r_minimal_node_msgs/package.xml 	new file:   src/main.rs 	renamed:    r2r_minimal_node_msgs/srv/HelloWorld.srv -> srv/HelloWorld.srv

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use r2r;
+{$ if use_additonal_msgs -%}
+use r2r::{{additonal_msgs}};
+{% endif -%}
+use futures::{StreamExt, future};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let ctx = r2r::Context::create()?;
+    let mut node = r2r::Node::create(ctx, "testnode", "")?;
+
+    println!("node name: {}", node.name()?);
+    println!("node fully qualified name: {}", node.fully_qualified_name()?);
+
+    node.params.lock().unwrap().iter().for_each(|(k,v)| {
+        println!("{} - {:?}", k, v.value);
+    });
+
+    loop {
+        node.spin_once(std::time::Duration::from_millis(100));
+    }
+}
